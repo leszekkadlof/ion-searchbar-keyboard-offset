3022421154bc3f90b337980e1de629bb31eb0fb3
lesk <les@yoohoomedia.com.au>
2019-05-24 10:04:05 +1000

    initial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-})
+               selector   : 'app-home',
+               templateUrl: 'home.page.html',
+               styleUrls  : ['home.page.scss'],
+           })
 export class HomePage {
+    items: string[] = [];
+    selectedTab: string;
 
+    constructor() {
+        for (let x = 0; x < 50; x++) {
+            this.items.push('' + x);
+        }
+    }
+
+    onTabChange(tabName: string) {
+
+    }
 }
